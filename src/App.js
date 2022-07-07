edb06995f886e92d773cb02c4565d4879660ee15
k-kha <kevinkha7@gmail.com>
2022-07-06 22:56:09 -0400

    Added name with link to GitHub

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a
+          className="App-link"
+          href="https://github.com/k-kha"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Kevin Kha
+        </a>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
